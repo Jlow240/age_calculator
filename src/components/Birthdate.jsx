77473664a2fd0b21eb87ff4696d563425dc5ee1f
Juan Luis Orozco <juanluis.ow@gmail.com>
2023-04-26 02:04:04 -0600

    Eliminacion de logica

--- src/components/Birthdate.jsx
+++ src/components/Birthdate.jsx
@@ -3,35 +3,18 @@ import './styles/Birthdate.css'
 
 
 const Birthdate = ({ handleSubmit }) => {
-
-    var input = document.getElementById('day');
-    input.addEventListener('input', function () {
-        if (this.value.length > 2)
-            this.value = this.value.slice(0, 2);
-    })
-    var input2 = document.getElementById('month');
-    input2.addEventListener('input2', function () {
-        if (this.value.length > 2)
-            this.value = this.value.slice(0, 2);
-    })
-    var input3 = document.getElementById('year');
-    input3.addEventListener('input3', function () {
-        if (this.value.length > 4)
-            this.value = this.value.slice(0, 4);
-    })
-
     return (
         <form className='birthdate' onSubmit={handleSubmit}>
             <div className='input__div'>
-                <label htmlFor="">DAY</label><input className='birth__input' type='number' id='day' min='1' max='31' placeholder='DD'></input>
+                <label htmlFor="">DAY</label><input required  className='birth__input' type='number' id='day' min='1' max='31' placeholder='DD'></input>
             </div>
 
             <div className='input__div'>
-                <label htmlFor="">MONTH</label><input className='birth__input' type='number' id='month' min='1' max='12' placeholder='MM'></input>
+                <label htmlFor="">MONTH</label><input required className='birth__input' type='number' id='month' min='1' max='12' placeholder='MM'></input>
             </div>
 
             <div className='input__div'>
-                <label htmlFor="">YEAR</label><input className='birth__input' type='number' id='year' placeholder='YYYY'></input>
+                <label htmlFor="">YEAR</label><input  required className='birth__input' type='number' id='year' placeholder='YYYY'></input>
             </div>
 
             <div className='age_arrow'>
