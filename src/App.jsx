df9d72935799cfc790989494ab29fef6e8c4fe61
Juan Luis Orozco <juanluis.ow@gmail.com>
2023-04-26 01:45:56 -0600

    Actualizacion de CSS

--- src/App.jsx
+++ src/App.jsx
@@ -14,28 +14,27 @@ function App() {
     setBirthday(e.target.day.value )
     setBirthmonth(e.target.month.value )
     setBirthyear(e.target.year.value )
-    e.target.reset()
   }
 
-  const birth =  new Date(`${ birthyear}, ${birthmonth}, ${birthday} `)
+  const birth =  new Date(`${birthyear}, ${birthmonth}, ${birthday}`)
   
-  let dateDiference = (actualDate - birth)
-  let diferenciaEnDias = Math.floor(dateDiference / (1000 * 60 * 60 * 24));
-  let years = Math.floor(diferenciaEnDias / 365);
-  let months = Math.floor((diferenciaEnDias % 365) / 30); 
-  let days = Math.floor((diferenciaEnDias % 365) % 30);
+  let dateDifference = (actualDate - birth)
+  let differenceInDays = Math.floor(dateDifference / (1000 * 60 * 60 * 24));
+  let years = Math.floor(differenceInDays / 365);
+  let months = Math.floor((differenceInDays % 365) / 30); 
+  let days = Math.floor((differenceInDays % 365) % 30);
 
   return (
     <main className="App">
       <article className='ageCounter'>
         <Birthdate handleSubmit={handleSubmit}/>
-        <div className='line'><div className='line'></div></div>
+        <div className='line'></div>
         
-        <div className='age_counter'>
+        <div className='age_display'>
           <ul className='age_list'>
-            <li><span>{years }</span> years</li>
-            <li><span>{months}</span> months</li>
-            <li><span>{days}</span> days</li>
+            <li><span>{years ? years : '--'}</span> years</li>
+            <li><span>{months ? months : '--'}</span> months</li>
+            <li><span>{days ? days : '--'}</span> days</li>
           </ul>
         </div>
 
--- src/components/Birthdate.jsx
+++ src/components/Birthdate.jsx
@@ -3,14 +3,31 @@ import './styles/Birthdate.css'
 
 
 const Birthdate = ({ handleSubmit }) => {
+
+    var input = document.getElementById('day');
+    input.addEventListener('input', function () {
+        if (this.value.length > 2)
+            this.value = this.value.slice(0, 2);
+    })
+    var input2 = document.getElementById('month');
+    input2.addEventListener('input2', function () {
+        if (this.value.length > 2)
+            this.value = this.value.slice(0, 2);
+    })
+    var input3 = document.getElementById('year');
+    input3.addEventListener('input3', function () {
+        if (this.value.length > 4)
+            this.value = this.value.slice(0, 4);
+    })
+
     return (
         <form className='birthdate' onSubmit={handleSubmit}>
             <div className='input__div'>
-                <label htmlFor="">DAY</label><input className='birth__input' type='number' id='day' placeholder='DD'></input>
+                <label htmlFor="">DAY</label><input className='birth__input' type='number' id='day' min='1' max='31' placeholder='DD'></input>
             </div>
 
             <div className='input__div'>
-                <label htmlFor="">MONTH</label><input className='birth__input' type='number' id='month' placeholder='MM'></input>
+                <label htmlFor="">MONTH</label><input className='birth__input' type='number' id='month' min='1' max='12' placeholder='MM'></input>
             </div>
 
             <div className='input__div'>
@@ -19,7 +36,6 @@ const Birthdate = ({ handleSubmit }) => {
 
             <div className='age_arrow'>
                 <button className='age_btn'><i className='bx bxs-down-arrow-circle'></i></button>
-                <hr />
             </div>
         </form>
     )
